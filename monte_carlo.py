e1cac0c57c42b02cea0980d6c9d58d9f253df386
Psmyth06 <psmyth1023@gmail.com>
2026-08-09 19:46:04 +0100

    Calculate simulated stock price

--- monte_carlo.py
+++ monte_carlo.py
@@ -12,3 +12,10 @@ import numpy as np
 random_number = np.random.normal(0, 1)
 
 print("Random shock:", random_number)
+
+simulated_return = annual_return + volatility * random_number
+
+future_price = starting_price * (1 + simulated_return)
+
+print("Simulated return:", simulated_return)
+print("Future stock price:", future_price)
